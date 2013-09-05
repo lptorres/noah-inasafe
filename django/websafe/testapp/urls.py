f3a51b90f915f5893e3c24a571585c57af206dbe
lptorres <luisantonio.p.torres@gmail.com>
2013-09-05 09:42:26 +0800

    Removed the webandgis directory and other unnecessary directories and their contents

--- django/websafe/testapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from testapp.views import MyCreateView, MyDetailView
-
-urlpatterns = patterns('',
-    url(r'^create/$', MyCreateView.as_view(), name='my_create'),
-    url(r'^(?P<slug>[\w-]+)/$', MyDetailView.as_view(), name='my_detail'),
-)
